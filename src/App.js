4c82da8e637b4873d8f983c2916a9135309150c2
mcdylanb <dylansatgalab@gmail.com>
2020-06-26 18:24:22 +0800

    Creates initial data inside App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,108 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
+let data = [
+  {
+    network: "globe-postpaid",
+    numbers: [
+      "09173",
+      "09175",
+      "09176",
+      "09178",
+      "09253",
+      "09255",
+      "09256",
+      "09257",
+      "09258"
+    ]
+  },
+  {
+    network: "globe",
+    numbers: ["0817"]
+  },
+  {
+    network: "sun",
+    numbers: [
+      "0922",
+      "0923",
+      "0924",
+      "0925",
+      "0931",
+      "0932",
+      "0933",
+      "0934",
+      "0940",
+      "0941",
+      "0942",
+      "0943",
+      "0973",
+      "0974"
+    ]
+  },
+  {
+    network: "tnt",
+    numbers: ["0907", "0909", "0910", "0912", "0930", "0938"]
+  },
+  {
+    network: "smart",
+    numbers: [
+      "0908",
+      "0918",
+      "0919",
+      "0920",
+      "0921",
+      "0928",
+      "0946",
+      "0948",
+      "0950",
+      "0929",
+      "0939",
+      "0946",
+      "0947",
+      "0949",
+      "0998",
+      "0999"
+    ]
+  },
+  {
+    network: "globe tm",
+    numbers: [
+      "0905",
+      "0906",
+      "0915",
+      "0916",
+      "0917",
+      "0926",
+      "0927",
+      "0935",
+      "0936",
+      "0937",
+      "0945",
+      "0955",
+      "0956",
+      "0965",
+      "0966",
+      "0967",
+      "0975",
+      "0977",
+      "0978",
+      "0979",
+      "0995",
+      "0996",
+      "0997"
+    ]
+  }
+];
+const globe = data[0].numbers;
+console.log(data);
+console.log(data[0].network);
+console.log(globe);
+console.log(globe.find(e => e === "0905"));
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      test
+      <p>test again</p>
     </div>
   );
 }
